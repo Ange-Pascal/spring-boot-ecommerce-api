b19d78d50612dd8301d1a4c2a6703c2cd39cc128
Ange-Pascal <angeakoumoua@gmail.com>
2025-08-20 09:14:02 +0000

    JPA and H2 configuration

---
+++ src/main/java/com/ecom/ecom/application/UserRepository.java
@@ -0,0 +1,7 @@
+package com.ecom.ecom.application;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {}
